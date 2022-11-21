f16dcab0aafa0228eb6417471b4f9ec6da755cba
Jinchuriki <mhrabby17@gmail.com>
2022-11-21 21:38:02 +0600

    Google search verification

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="google-site-verification" content="6hZus_hKtAfltqFb5vDctU3nRP4IzYSvZ9pONS2nDb4" />
         <script>window.Laravel = { csrfToken: '{{ csrf_token() }}'};</script>
 
         <title>Track My Money | The most useful income-expense tracker</title>
